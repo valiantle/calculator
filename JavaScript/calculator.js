41e3af3e086a7c24b25bfee0bd21159da3e7b95f
valiantleprano <valiant.leprano@gmail.com>
2025-02-12 17:16:38 +0000

    13.4

--- JavaScript/calculator.js
+++ JavaScript/calculator.js
@@ -1,1 +1,12 @@
-let button1 
+let outputRow = "";
+
+function addNum(number) {
+    outputRow += number;
+    document.getElementById("Poutput").innerText = outputRow;
+}
+
+function clearRow() {
+    document.getElementById("Poutput").innerText = "";
+}
+// текст заменяется проблемо но не исчезает на совсем почемуто ????
+// текст заменяется проблемо но не исчезает на совсем почемуто ????
